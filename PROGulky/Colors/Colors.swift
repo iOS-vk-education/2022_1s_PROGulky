1a392ca74530afe8db1fd1680d7f676ef6284791
K1selev <serge2000k@gmail.com>
2022-11-08 17:25:31 +0300

    refactor code

---
+++ PROGulky/Colors/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  PROGulky
+//
+//  Created by Сергей Киселев on 08.11.2022.
+//
+
+import Foundation
---
+++ PROGulky/TextConstants/TextConstants.swift
@@ -0,0 +1,23 @@
+//
+//  TextConstants.swift
+//  PROGulky
+//
+//  Created by Сергей Киселев on 08.11.2022.
+//
+
+import Foundation
+
+class TextConstants: NSObject {
+    static let titleProfile = "Профиль"
+    static let titleAccount = "Аккаунт"
+    static let titleOthers = "Другое"
+    static let titleBeGuide = "Стать \nэкскурсоводом"
+    static let titlePersonalData = "Персональные данные"
+    static let titleAchievements = "Достижения"
+    static let titleHistory = "История прогулок"
+    static let titleContactUs = "Свяжитесь с нами"
+    static let titlePrivacyPolicy = "Политика конфиденциальности"
+    static let titleSignOut = "Выйти из аккаунта"
+    static let titleUserName = "Имя пользователя"
+    static let titleUserStatus = "статус пользователя"
+}
